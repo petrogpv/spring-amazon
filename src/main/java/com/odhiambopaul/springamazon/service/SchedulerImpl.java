7d43912b659ecc22c2a79cd71d7a7813ceea8420
Petro Gordeichuk <petro_gordeichuk@epam.com>
2021-07-23 10:50:55 +0300

    add profile

--- src/main/java/com/odhiambopaul/springamazon/service/SchedulerImpl.java
+++ src/main/java/com/odhiambopaul/springamazon/service/SchedulerImpl.java
@@ -11,9 +11,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Profile("withsns")
 @Service
 @RequiredArgsConstructor
 @Slf4j
